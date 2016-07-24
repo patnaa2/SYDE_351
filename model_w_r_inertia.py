d5a601cb72b7b0c5037d21eb4c48cb442d414333
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-07-24 19:05:56 -0400

    fixed some stuff

--- model_w_r_inertia.py
+++ model_w_r_inertia.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from math import tan, cos, sin, pi
-from scipy.integrate import odeint
+from scipy.integrate import odeint, simps
 
 ##############
 ## y0 = yk 
@@ -16,47 +16,53 @@ def model(y, t):
     
     # constants
     m = 0.68 # Mass of pendulum
-    k = 300 # Stiffness of Spring
+    k = 30 # Stiffness of Spring
     Y = 0
     b = 0.5 # Torsional Resistance
-    R = 0.8  # Friction
+    R = 8 # Friction
     L = 0.61  # Length of Pendulum
     g = 9.81 # Gravitional acceleration
 
     # in between terms
     disp = (yk - Y)
 
-    d_yk = vy + ( (L**2) * sin(theta) * cos(theta) * vx) 
-    d_theta = L * cos(theta) * vx 
+    d_yk = vy + ( (L**2) * tan(theta) * vx) 
+    d_theta = vx / (L * cos(theta)) 
     d_vy = g + ( -R * d_yk - k * yk)
     d_vx = (L * cos(theta)) / ( 1 +  ((L**4)/12) * (cos(theta) ** 2)) * ((L**3 / 12 * d_theta * sin(theta) * vx) + (L * sin(theta) * d_vy) - (L * sin(theta) * g) - (d_theta * b / m))
 
     return [d_yk, d_theta, d_vx, d_vy]
 
-time = np.linspace(0.0, 20.0, 100)
-yinit = [0, pi/4, 0, 0]
+time = np.linspace(0.0, 20.0, 1000)
+yinit = [0.03, pi/8, 0, 0]
 y = odeint(model, yinit, time)
+#import pdb ; pdb.set_trace()
+plt.figure(1)
 
+plt.subplot(211)
 plt.plot(time, y[:,0], label="yk")
 plt.xlabel('t')
-plt.ylabel('Motion of Spring')
+plt.ylabel('Displacement of Spring')
 plt.legend()
-plt.show()
 
-plt.plot(time, y[:,1], label="theta")
+plt.subplot(212)
+plt.plot(time, y[:,3], label="vy")
 plt.xlabel('t')
-plt.ylabel('Angle of rotation')
+plt.ylabel('Velocity of Pendulum in Y')
 plt.legend()
 plt.show()
 
+plt.figure(2)
+
+plt.subplot(211)
 plt.plot(time, y[:,2], label="vx")
 plt.xlabel('t')
 plt.ylabel('Velocity of Pendulum in X')
 plt.legend()
-plt.show()
 
-plt.plot(time, y[:,3], label="vy")
+plt.subplot(212)
+plt.plot(time, y[:,1], label="theta")
 plt.xlabel('t')
-plt.ylabel('Velocity of Pendulum in Y')
+plt.ylabel('Angle of rotation')
 plt.legend()
 plt.show()
