f9bf957308f9cf77c90250ac9873d4029bc3ac8d
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-07-25 00:24:16 -0400

    fixed proper mtf usage

--- model_w_r_inertia.py
+++ model_w_r_inertia.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from math import tan, cos, sin, pi
-from scipy.integrate import odeint, simps
+from scipy.integrate import odeint, simps, cumtrapz 
 
 ##############
 ## y0 = yk 
@@ -16,10 +16,10 @@ def model(y, t):
     
     # constants
     m = 0.68 # Mass of pendulum
-    k = 30 # Stiffness of Spring
+    k = 300 # Stiffness of Spring
     Y = 0
     b = 0.5 # Torsional Resistance
-    R = 8 # Friction
+    R = 0.8 # Friction
     L = 0.61  # Length of Pendulum
     g = 9.81 # Gravitional acceleration
 
@@ -29,14 +29,22 @@ def model(y, t):
     d_yk = vy + ( (L**2) * tan(theta) * vx) 
     d_theta = vx / (L * cos(theta)) 
     d_vy = g + ( -R * d_yk - k * yk)
-    d_vx = (L * cos(theta)) / ( 1 +  ((L**4)/12) * (cos(theta) ** 2)) * ((L**3 / 12 * d_theta * sin(theta) * vx) + (L * sin(theta) * d_vy) - (L * sin(theta) * g) - (d_theta * b / m))
 
+    # the derivative causality is resolved here, so adding some in between
+    # terms for easier debugging
+    e_21 = tan(theta) * (d_vy - g) # comes from the left side of bg
+    e_24 = d_theta * b  # torsional resistance
+    e_22 = d_theta * tan(theta) * vx / (12 * (cos(theta)**2))
+    factor = 1 / (1 + (1 / ( 12 * (cos(theta)**2))))
+
+    d_vx = factor * (e_21 - e_22 - e_24)
     return [d_yk, d_theta, d_vx, d_vy]
 
-time = np.linspace(0.0, 20.0, 1000)
-yinit = [0.03, pi/8, 0, 0]
+time = np.linspace(0.0, 10.0, 10000)
+yinit = [0.5, pi/8, 0, 0]
 y = odeint(model, yinit, time)
-#import pdb ; pdb.set_trace()
+ped_y = cumtrapz(y[:,3], time, initial=0)
+ped_x = cumtrapz(y[:,2], time, initial=0)
 plt.figure(1)
 
 plt.subplot(211)
@@ -46,18 +54,18 @@ plt.ylabel('Displacement of Spring')
 plt.legend()
 
 plt.subplot(212)
-plt.plot(time, y[:,3], label="vy")
+plt.plot(time, ped_y, label="p_y")
 plt.xlabel('t')
-plt.ylabel('Velocity of Pendulum in Y')
+plt.ylabel('Displacement of Pendulum in Y')
 plt.legend()
 plt.show()
 
 plt.figure(2)
 
 plt.subplot(211)
-plt.plot(time, y[:,2], label="vx")
+plt.plot(time, ped_x, label="p_x")
 plt.xlabel('t')
-plt.ylabel('Velocity of Pendulum in X')
+plt.ylabel('Displacement of Pendulum in X')
 plt.legend()
 
 plt.subplot(212)
