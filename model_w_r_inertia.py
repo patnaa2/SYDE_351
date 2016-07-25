a13221e275dc73a2a73942d3d7824bc8a788bd79
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2016-07-25 13:21:42 -0400

    fix one of the state equations

--- model_w_r_inertia.py
+++ model_w_r_inertia.py
@@ -15,20 +15,20 @@ def model(y, t):
     yk, theta, vx, vy = y
     
     # constants
-    m = 0.68 # Mass of pendulum
-    k = 300 # Stiffness of Spring
-    Y = 0
-    b = 0.5 # Torsional Resistance
-    R = 0.8 # Friction
+    m = 0.408 # Mass of pendulum
+    k = 100 # Stiffness of Spring
+    b = 1.2 # Torsional Resistance
+    R = 2 # Friction
     L = 0.61  # Length of Pendulum
     g = 9.81 # Gravitional acceleration
+    Y = 0.5 
 
     # in between terms
     disp = (yk - Y)
 
-    d_yk = vy + ( (L**2) * tan(theta) * vx) 
+    d_yk = vy + ((tan(theta) * vx)) 
     d_theta = vx / (L * cos(theta)) 
-    d_vy = g + ( -R * d_yk - k * yk)
+    d_vy = g + (( -R * d_yk - k * yk)/m)
 
     # the derivative causality is resolved here, so adding some in between
     # terms for easier debugging
@@ -41,10 +41,15 @@ def model(y, t):
     return [d_yk, d_theta, d_vx, d_vy]
 
 time = np.linspace(0.0, 10.0, 10000)
-yinit = [0.5, pi/8, 0, 0]
+yinit = [0, pi/2, 0, 0]
 y = odeint(model, yinit, time)
+
+# the state equations give us velocity
+# integrate again to get displacement
+# our variable of interest
 ped_y = cumtrapz(y[:,3], time, initial=0)
 ped_x = cumtrapz(y[:,2], time, initial=0)
+
 plt.figure(1)
 
 plt.subplot(211)
@@ -54,23 +59,23 @@ plt.ylabel('Displacement of Spring')
 plt.legend()
 
 plt.subplot(212)
-plt.plot(time, ped_y, label="p_y")
+plt.plot(time, y[:,1], label="theta")
 plt.xlabel('t')
-plt.ylabel('Displacement of Pendulum in Y')
+plt.ylabel('Angle of rotation')
 plt.legend()
 plt.show()
 
 plt.figure(2)
 
 plt.subplot(211)
-plt.plot(time, ped_x, label="p_x")
+plt.plot(time, ped_x, label="yx")
 plt.xlabel('t')
 plt.ylabel('Displacement of Pendulum in X')
 plt.legend()
 
 plt.subplot(212)
-plt.plot(time, y[:,1], label="theta")
+plt.plot(time, ped_y, label="yp")
 plt.xlabel('t')
-plt.ylabel('Angle of rotation')
+plt.ylabel('Displacement of Pendulum in Y')
 plt.legend()
 plt.show()
